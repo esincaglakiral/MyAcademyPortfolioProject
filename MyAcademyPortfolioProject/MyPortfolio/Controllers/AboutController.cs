7a9ed6ce072616af8975d64983c9b72579ccfdd8
Esin Çağla KIRAL <caglakiral@gmail.com>
2024-03-21 23:33:23 +0300

    first commit
    Update Project

--- MyAcademyPortfolioProject/MyPortfolio/Controllers/AboutController.cs
+++ MyAcademyPortfolioProject/MyPortfolio/Controllers/AboutController.cs
@@ -16,9 +16,57 @@ namespace MyPortfolio.Controllers
             return View(values); // değeri döndürmek için
         }
 
-        // db nesnesi aracılığı ile listeleme işlemini burada yaptık, View içerisinde bu listeleme işlemini görmek istersem:
-        // 1- index'e gelip go to view dersem hakkımda sayfasının kod içeriğe gider (html sayfası)
-        // 2- ilgili sayfada (html sayfası) en başa @model List<MyPortfolio.Models.TblAbouts> ekleriz. 
+        [HttpGet]  // Sadece sayfayı görüntüleriz
+        public ActionResult AddAbout()
+        {
+            return View();
+        }
+
+        [HttpPost] //içerisinde form oluştururuz ve butona tıkladığımız zaman post metodu çalışıcaktır.
+        public ActionResult AddAbout(TblAbouts abouts)
+        {
+            db.TblAbouts.Add(abouts); //db nesnesine tblabouts tablosundan abouts parametresini add metoduyla verdik yani ekleme yaptıkça tabloya da eklicek
+            db.SaveChanges(); // bunu yapmazsak ekleme işlemi gerçekleşmez
+            return RedirectToAction("Index"); // index action'una gönderir
+        }
+
+        public ActionResult DeleteAbout(int id)
+        {
+           var about =  db.TblAbouts.Find(id); //İd den gelen değerle birlikte tblabout tablosundan gelen değeri buldu ve about değişkenine atadı
+           db.TblAbouts.Remove(about); // bulduğu değeri Remove metodu ile siler yani önce id den gelen değeri bulur sonra siler
+           db.SaveChanges();  // işlemi kaydediyoruz
+           return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+
+        public ActionResult UpdateAbout(int id) // önce id ye bağlı değerleri getirmesi için (formu açınca o idye bağlı değerlerin gelmesi) httpget metodu 
+        {
+            var about = db.TblAbouts.Find(id);
+            return View(about);
+        }
+
+
+        [HttpPost]
+        public ActionResult UpdateAbout(TblAbouts abouts)  // güncelleme işlemi yapmak için
+        {
+            var value = db.TblAbouts.Find(abouts.AboutId); // value değişkenine aboutid den gelen değerleri bulup ata
+            value.ImageUrl = abouts.ImageUrl;  // parametrelerime atamalarımı yaptım
+            value.Title = abouts.Title;
+            value.Description = abouts.Description;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
 
     }
 }
+
+// db nesnesi aracılığı ile listeleme işlemini burada yaptık, View içerisinde bu listeleme işlemini görmek istersem:
+// 1- index'e gelip go to view dersem hakkımda sayfasının kod içeriğe gider (html sayfası)
+// 2- ilgili sayfada (html sayfası) en başa @model List<MyPortfolio.Models.TblAbouts> ekleriz. 
+// 3- Controller tarafında değişkenimize (values) liste türünde bir değer atadığımız için view tarafında da
+// modelimizi liste türünde ( @model List<MyPortfolio.Models.TblAbouts> ) veririz.
--- MyAcademyPortfolioProject/MyPortfolio/Controllers/CategoryController.cs
+++ MyAcademyPortfolioProject/MyPortfolio/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyPortfolio.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,50 @@ namespace MyPortfolio.Controllers
 {
     public class CategoryController : Controller
     {
-        // GET: Category
+       MyAcademyPortfolioProjectEntities db = new MyAcademyPortfolioProjectEntities();  //db nesnesi türetiriz
         public ActionResult Index()
+        {
+            var values = db.TblCategories.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory() 
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult AddCategory(TblCategories categories) 
+        {
+            db.TblCategories.Add(categories);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));  // return RedirectToAction("Index"); ifadesiyle bu ifade aynı anlamda ikisi de kullanılabilir.
+        }
+
+        public ActionResult DeleteCategory(int id) 
+        {
+            var category = db.TblCategories.Find(id); 
+            db.TblCategories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpGet]
+        public ActionResult UpdateCategory(int id)  // sadece id ye bağlı değerlerimizi getirdik
+        {
+            var value = db.TblCategories.Find(id); // var category = db.TblCategories.FirstOrDefault(x => x.CategoryId == id); de diyebilirdik aynı işlem
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(TblCategories category)  // gelen değerlerde güncelleme işlemi yapması için gerekli metodu yazdık
+        {
+            var value = db.TblCategories.Find(category.CategoryId);
+            value.CategoryName = category.CategoryName;
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
     }
-}
+} 
